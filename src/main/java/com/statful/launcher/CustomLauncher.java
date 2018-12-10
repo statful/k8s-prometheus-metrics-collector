945dd6a1553783ded11e7543d012dff30a968360
gabrielmpinto <gabrielpinto1991@gmail.com>
2018-12-10 14:32:43 +0000

    chore(NA): Configuration for metric aggregations

--- src/main/java/com/statful/launcher/CustomLauncher.java
+++ src/main/java/com/statful/launcher/CustomLauncher.java
@@ -1,5 +1,6 @@
 package com.statful.launcher;
 
+import com.statful.client.Aggregation;
 import com.statful.client.StatfulMetricsFactoryImpl;
 import com.statful.client.StatfulMetricsOptions;
 import com.statful.collector.k8s.clients.KubeApi;
@@ -10,6 +11,7 @@ import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Objects.nonNull;
@@ -20,6 +22,9 @@ public class CustomLauncher extends Launcher implements Loggable {
     private String statfulHost;
     private boolean statfulDryRun;
     private final List<Pair<String, String>> tags = new ArrayList<>(1);
+    private final List<Aggregation> timerAgg = new ArrayList<>();
+    private final List<Aggregation> counterAgg = new ArrayList<>();
+    private final List<Aggregation> gaugeAgg = new ArrayList<>();
 
     public static void main(String[] args) {
         new CustomLauncher().dispatch(args);
@@ -29,7 +34,7 @@ public class CustomLauncher extends Launcher implements Loggable {
     public void beforeStartingVertx(VertxOptions options) {
         parseConfigSystemProperties();
 
-        options.setMetricsOptions(new StatfulMetricsOptions()
+        final StatfulMetricsOptions metricsOptions = new StatfulMetricsOptions()
                 .setEnabled(true)
                 .setDryrun(statfulDryRun)
                 .setHost(statfulHost)
@@ -37,18 +42,45 @@ public class CustomLauncher extends Launcher implements Loggable {
                 .setNamespace("kubernetes")
                 .setTags(tags)
                 .setFlushSize(250)
-                .setFlushInterval(10000)
-                .setFactory(new StatfulMetricsFactoryImpl()));
+                .setFlushInterval(10000);
+
+        if (!timerAgg.isEmpty()) {
+            metricsOptions.setTimerAggregations(timerAgg);
+        }
+
+        if (!counterAgg.isEmpty()) {
+            metricsOptions.setCounterAggregations(counterAgg);
+        }
+
+        if (!gaugeAgg.isEmpty()) {
+            metricsOptions.setGaugeAggregations(gaugeAgg);
+        }
+
+        metricsOptions.setFactory(new StatfulMetricsFactoryImpl());
+
+        options.setMetricsOptions(metricsOptions);
     }
 
     private void parseConfigSystemProperties() {
         statfulToken = System.getProperty("statful.token");
         statfulHost = System.getProperty("statful.host", "api.statful.com");
         statfulDryRun = Boolean.valueOf(System.getProperty("statful.dryrun", String.valueOf(false)));
+        collectAggregationProperty("statful.timer.agg");
+        collectAggregationProperty("statful.counter.agg");
+        collectAggregationProperty("statful.gauge.agg");
         String statfulEnvironment = System.getProperty("statful.environment");
 
         if (nonNull(statfulEnvironment)) {
             tags.add(new Pair<>("environment", statfulEnvironment));
+        }
+    }
+
+    private void collectAggregationProperty(String propertyName) {
+        final String property = System.getProperty(propertyName, "");
+        if (!property.isEmpty()) {
+            Arrays.stream(property.split(","))
+                    .map(Aggregation::valueOf)
+                    .forEach(timerAgg::add);
         }
     }
 
